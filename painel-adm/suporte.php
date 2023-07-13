760387b1bbe74f3e7bfa2029fdf92850d7f5e5d5
Luis Fiabani <luisfiabani@gmail.com>
2023-07-13 10:28:34 -0300

    Alterando dados do suporte

--- painel-adm/suporte.php
+++ painel-adm/suporte.php
@@ -1,10 +1,4 @@
 <h1>Suporte</h1>
-
-<p>Está com problemas para utilizar o sistema?</p>
-<p><a href="#">Clique aqui</a> e confira nossas vídeo aulas sobre o sistema.</p>
-<p>Caso não tenha conseguido resolver seu problema, entre em contato com o suporte.</p>
-
-<h3>Suporte SystemPsi</h3>
-<P>Entre em contato: <a href="mailto:suporte.systempsi@gmail.com">suporte.sistemapsico@gmail.com</a>.</P>
+<P>Entre em contato: <a href="mailto:contato@systempsi.xyz">contato@systempsi.xyz</a>.</P>
 <p>Telefone (whatsapp): (51) 99282-5316.</p>
 <p>Funcionamento de segunda à sexta das 08h até as 17h.</p>
